3ad60f84950f4aebbc4273ca1aa5df9a0a8d9558
Miel <miel.noelanders@qnh.Be>
2018-04-16 14:40:38 +0200

    654

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { MusicComponent } from './music/music.component';
+import { BandComponent } from './band/band.component';
+import { BioComponent } from './bio/bio.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    MusicComponent,
+    BandComponent,
+    BioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
